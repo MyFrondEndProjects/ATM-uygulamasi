4119c9c0a578081caa32502c191bc0a7aced378d
MyFrondEndProjects <usscrean1@outlook.com>
2024-03-14 02:45:20 +0300

    Dosyalar güncellendi
    Dosyalar güncellendi

--- Js/App.js
+++ Js/App.js
@@ -2,6 +2,17 @@ let Powerbutton=document.querySelector("#Powerbutton");
 let Walcometext=document.querySelector(".Walcome-text");
 let Onbutton=document.querySelector(".Onbutton");
 let walcomeimg=document.querySelector(".Walcome");
+let Loginform=document.querySelector("#Loginform");
+let blackpanel=document.querySelector(".blackpanel");
+let activepanel=document.querySelector("#Loginform");
+
+
+const data =[
+    {
+        Kartnumarası:"123",
+        sifre:"12345678910",
+    }
+]
 function active ()
 {
 Walcometext.remove();
@@ -10,6 +21,79 @@ walcomeimg.src="İmg/Hos-geldiniz.png";
 
 
 
+let kartnumarasi=document.createElement("input");
+kartnumarasi.type="text";
+kartnumarasi.classList.add("kartnumarasi");
+kartnumarasi.placeholder="Kart numaranızı giriniz";
+
+let sifre=document.createElement("input");
+sifre.type="password";
+sifre.classList.add("sifre");
+sifre.placeholder="Sifrenizi giriniz";
+
+let girisbuton=document.createElement("button");
+girisbuton.classList.add("girisbuton");
+girisbuton.textContent="Giris Yap"
+girisbuton.href="#";
+
+
+let Kartno=document.createElement("p");
+Kartno.classList.add("Kartno");
+Kartno.textContent="Kart numarası";
+
+
+let Pass=document.createElement("p");
+Pass.classList.add("Pass");
+Pass.textContent="Sifre";
+
+Loginform.appendChild(kartnumarasi);
+Loginform.appendChild(sifre);
+Loginform.appendChild(girisbuton);
+Loginform.appendChild(Kartno);
+Loginform.appendChild(Pass);
+
+function giris()
+{
+    for(let i=0; i<data.length; i++)
+    {
+         if(kartnumarasi.value==data[i].Kartnumarası && sifre.value==data[i].sifre)
+         {
+             alert("Giriş başarılı");
+         }
+       else
+       {
+        alert("Kart numarası ya da sifre hatalı");
+       }
+    }
+}
+girisbuton.addEventListener('click',giris);
 }
 
+
+
+function Off()
+{
+    while (blackpanel.firstChild) {
+        blackpanel.removeChild(blackpanel.firstChild); 
+    }
+    while (Loginform.firstChild) {
+        Loginform.removeChild(Loginform.firstChild); 
+        
+    }
+    Onbutton.remove();
+    
+     let activebutton=document.createElement("a");
+     activebutton.classList.add("Onbutton");
+     
+     let imgbuton=document.createElement("img");
+     imgbuton.classList="ActiveButon";
+     imgbuton.src="İmg/Onbutton.png";
+
+     
+
+     location.reload();
+}
+
+
+Onbutton.addEventListener('click',Off);
 Powerbutton.addEventListener('click',active);
